33c20d6a53b560801d9889df999e7843d52103b5
Bedknobs <noeliareysilva@gmail.com>
2024-02-22 13:11:20 +0100

    navbar fixed

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -2,32 +2,6 @@ import "./Filter.css";
 
 export const Filter = () => {
   return (
-    <div className="filterContainer">
-      <form>
-        <p>Categoría</p>
-        <select className="form-select" aria-label="Default select example">
-          <option selected></option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <p>Modelo</p>
-        <select className="form-select" aria-label="Default select example">
-          <option selected></option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <p>Participantes</p>
-        <select className="form-select" aria-label="Default select example">
-          <option selected></option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <p>Edades de los participantes</p>
-        <input type="range" className="form-range" id="customRange1"></input>
-      </form>
-    </div>
+    <></>
   );
 };
