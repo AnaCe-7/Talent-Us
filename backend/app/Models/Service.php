628e6fdcd83bd9ce7bc7df0394e64a3dc4cc8ee8
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-02-22 18:51:31 +0100

    Create Service model and Services migration. Errors in the Service model file because the other models and migrations have not been created yet

---
+++ backend/app/Models/Service.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category',
+        'title',
+        'description',
+        'image',
+        'requirements',
+        'participants',
+        'id_user',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class, 'id_service', 'id');
+    }
+}
