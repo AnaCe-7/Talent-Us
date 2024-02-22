25854f90faf25bf5bdddc477d41678df25b78f25
Bedknobs <noeliareysilva@gmail.com>
2024-02-22 11:03:35 +0100

    reset frontend and add components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {Home} from "./components/Home/Home";
+import { Categories } from "./components/Categories/Categories";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Filter } from "./components/Filter/Filter";
+import { ServiceView } from "./components/ServiceView/ServiceView";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/categories" element={<Categories />}></Route>
+        <Route path="/filter" element={<Filter />}></Route>
+        <Route path="/service" element={<ServiceView />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/ServiceView/ServiceView.jsx
@@ -0,0 +1,3 @@
+export const ServiceView = () => {
+  return <div>ServiceView</div>;
+};
