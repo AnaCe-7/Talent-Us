0638d94f9cd65d708b44e136c4a6207e60ad1c8b
Bedknobs <noeliareysilva@gmail.com>
2024-02-22 14:10:28 +0100

    filter style done

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,32 +1,35 @@
-import "./Filter.css"
+import "./Filter.css";
 
 export const Filter = () => {
   return (
     <div className="filterContainer">
-      <form>
+      <h2 className="filterTitle">ÚNETE Y CREA TU PROPIO FUTURO</h2>
+      <form className="filterForm">
         <p>Categoría</p>
         <select className="form-select" aria-label="Default select example">
           <option selected></option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option value="jardineria">Jardinería</option>
+          <option value="informatica">Informática</option>
+          <option value="cocina">Cocina</option>
         </select>
         <p>Modelo</p>
         <select className="form-select" aria-label="Default select example">
           <option selected></option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option value="presencial">Presencial</option>
+          <option value="online">Online</option>
         </select>
         <p>Participantes</p>
         <select className="form-select" aria-label="Default select example">
           <option selected></option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option value="menosDeCinco">- 5</option>
+          <option value="entreCincoYDiez">5 - 10</option>
+          <option value="masDeDiez">+ 10</option>
         </select>
-        <p>Edades de los participantes</p>
-        <input type="range" className="form-range" id="customRange1"></input>
+        <div className="formBtnContainer">
+          <button type="send" className="filterFormBtn">
+            BUSCAR
+          </button>
+        </div>
       </form>
     </div>
   );
